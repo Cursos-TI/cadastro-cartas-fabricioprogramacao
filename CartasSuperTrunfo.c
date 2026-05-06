33df71fca591eac10a0c210b7f387db785287af4
fabricioprogramacao <programacao.githubb23@gmail.com>
2026-05-06 22:29:24 +0000

    terminei a implementação do jogo, agora é só testar e corrigir os bugs que aparecerem

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,45 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-printf("olá, mundo no codespace\n");
-printf("exemplo de edição de codigo\n");
+  char Estado;
+    char código_da_carta [20], Nome_da_cidade [25];
+    int população, Número_de_pontos_turísticos;
+    float Àrea, PIB;
+
+    printf("Carta 1:\n");
+    printf("Estado: ");
+    scanf("%c", &Estado);
+    printf("código: ");
+    scanf("%s", &código_da_carta);
+    printf("Nome da cidade: ");
+    scanf("%s", &Nome_da_cidade);
+    printf("população: ");
+    scanf("%d", &população);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &Número_de_pontos_turísticos);
+    printf("Área: ");
+    scanf("%f", &Àrea);
+    printf("PIB: ");
+    scanf("%f", &PIB);
+
+    printf("Carta 2:\n");
+    printf("Estado: ");
+    scanf(" %c", &Estado);
+    printf("código: ");
+    scanf("%s", &código_da_carta);
+    printf("Nome da cidade: ");
+    scanf("%s", &Nome_da_cidade);
+    printf("população: ");
+    scanf("%d", &população);
+    printf("Número de pontos turisticos: ");
+    scanf("%d", &Número_de_pontos_turísticos);
+    printf("Àrea: ");
+    scanf("%f", &Àrea);
+    printf("PIB: ");
+    scanf("%f", &PIB);
+
+
+
 
   // Área para entrada de dados
 
